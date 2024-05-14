dee425a103ea7db7209f385067b79ad971dc5a51
belljun3395 <195850@jnu.ac.kr>
2024-05-14 16:54:24 +0900

    전체 변수에 aws rds 변수 추가

--- variable.tf
+++ variable.tf
@@ -9,3 +9,15 @@ variable "aws_secret_key" {
   sensitive   = true
   description = "The secret key for the IAM root user"
 }
+
+variable "aws_rds_username" {
+  type        = string
+  default     = "root"
+  description = "The username for the RDS instance"
+}
+
+variable "aws_rds_password" {
+  type        = string
+  default     = "thisisrdspassword"
+  description = "The password for the RDS instance"
+}
