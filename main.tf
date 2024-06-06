4220dbb6a16ea2941e61f472706afd85741cbb22
belljun3395 <195850@jnu.ac.kr>
2024-06-06 11:32:12 +0900

    main에 추가된 변수 반영

--- main.tf
+++ main.tf
@@ -11,9 +11,12 @@ terraform {
 
 # AWS
 module "aws" {
-  source       = "./aws"
-  access_key   = var.aws_access_key
-  secret_key   = var.aws_secret_key
-  rds_username = var.aws_rds_username
-  rds_password = var.aws_rds_password
+  source                = "./aws"
+  access_key            = var.aws_access_key
+  secret_key            = var.aws_secret_key
+  rds_username          = var.aws_rds_username
+  rds_password          = var.aws_rds_password
+  kakao_admin_key       = var.kakao_admin_key
+  kako_app_redirect_uri = var.kako_app_redirect_uri
+  kakao_app_client_id   = var.kakao_app_client_id
 }
