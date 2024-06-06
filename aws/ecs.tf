fa7c4146375e4ac1427fd91c04cd17940625a334
belljun3395 <195850@jnu.ac.kr>
2024-06-06 11:30:39 +0900

    컨테이너 정의에 변수 추가

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -33,7 +33,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
         "essential" : true,
         "portMappings" : [
           {
-            "containerPort" : 80,
+            "containerPort" : 8080,
             "hostPort" : 0,
             "protocol" : "tcp"
           }
@@ -42,13 +42,121 @@ resource "aws_ecs_task_definition" "ecs_task" {
         "volumesFrom" : [],
         "secrets" : [
           {
-            "name" : "SECURE_STRING",
-            "valueFrom" : "${aws_ssm_parameter.secure_string.name}"
+            "name" : "TOKEN_SECRETKEY",
+            "valueFrom" : "${aws_ssm_parameter.token_secret_key.name}"
           },
           {
-            "name" : "STRING",
-            "valueFrom" : "${aws_ssm_parameter.string.name}"
-          }
+            "name" : "CORS_PATH_PATTERNS",
+            "valueFrom" : "${aws_ssm_parameter.cors_path_patterns.name}"
+          },
+          {
+            "name" : "CORS_ORIGIN_PATTERNS",
+            "valueFrom" : "${aws_ssm_parameter.cors_origin_patterns.name}"
+          },
+          {
+            "name" : "CORS_ALLOWED_METHODS",
+            "valueFrom" : "${aws_ssm_parameter.cors_allowed_methods.name}"
+          },
+          {
+            "name" : "CORS_ALLOWED_HEADERS",
+            "valueFrom" : "${aws_ssm_parameter.cors_allowed_headers.name}"
+          },
+          {
+            "name" : "CORS_EXPOSED_HEADERS",
+            "valueFrom" : "${aws_ssm_parameter.cors_exposed_headers.name}"
+          },
+          {
+            "name" : "CORS_ALLOW_CREDENTIALS",
+            "valueFrom" : "${aws_ssm_parameter.cors_allow_credentials.name}"
+          },
+          {
+            "name" : "CORS_MAX_AGE",
+            "valueFrom" : "${aws_ssm_parameter.cors_max_age.name}"
+          },
+          {
+            "name" : "COOKIE_DOMAIN",
+            "valueFrom" : "${aws_ssm_parameter.cookie_domain.name}"
+          },
+          {
+            "name" : "COOKIE_PATH",
+            "valueFrom" : "${aws_ssm_parameter.cookie_path.name}"
+          },
+          {
+            "name" : "COOKIE_MAX_AGE",
+            "valueFrom" : "${aws_ssm_parameter.cookie_max_age.name}"
+          },
+          {
+            "name" : "DB_HOSTNAME",
+            "valueFrom" : "${aws_ssm_parameter.db_hostname.name}"
+          },
+          {
+            "name" : "DB_USERNAME",
+            "valueFrom" : "${aws_ssm_parameter.db_username.name}"
+          },
+          {
+            "name" : "DB_PASSWORD",
+            "valueFrom" : "${aws_ssm_parameter.db_password.name}"
+          },
+          {
+            "name" : "S3_URL",
+            "valueFrom" : "${aws_ssm_parameter.s3_url.name}"
+          },
+          {
+            "name" : "S3_ACCESS_KEY",
+            "valueFrom" : "${aws_ssm_parameter.s3_access_key.name}"
+          },
+          {
+            "name" : "S3_SECRET_KEY",
+            "valueFrom" : "${aws_ssm_parameter.s3_secret_key.name}"
+          },
+          {
+            "name" : "S3_BUCKET_NAME",
+            "valueFrom" : "${aws_ssm_parameter.s3_bucket_name.name}"
+          },
+          {
+            "name" : "S3_REGION",
+            "valueFrom" : "${aws_ssm_parameter.s3_region.name}"
+          },
+          {
+            "name" : "MAX_ATTEMPTS",
+            "valueFrom" : "${aws_ssm_parameter.max_attempts.name}"
+          },
+          {
+            "name" : "BACK_OFF_PERIOD",
+            "valueFrom" : "${aws_ssm_parameter.back_off_period.name}"
+          },
+          {
+            "name" : "KAKO_HOST",
+            "valueFrom" : "${aws_ssm_parameter.kako_host.name}"
+          },
+          {
+            "name" : "KAKO_ADMIN_KEY",
+            "valueFrom" : "${aws_ssm_parameter.kako_admin_key.name}"
+          },
+          {
+            "name" : "KAKO_TOKEN_URI",
+            "valueFrom" : "${aws_ssm_parameter.kako_token_uri.name}"
+          },
+          {
+            "name" : "KAKO_TOKEN_INFO_URI",
+            "valueFrom" : "${aws_ssm_parameter.kako_token_info_uri.name}"
+          },
+          {
+            "name" : "KAKO_ME_INFO_URI",
+            "valueFrom" : "${aws_ssm_parameter.kako_me_info_uri.name}"
+          },
+          {
+            "name" : "KAKO_UNLINK_URI",
+            "valueFrom" : "${aws_ssm_parameter.kako_unlink_uri.name}"
+          },
+          {
+            "name" : "KAKO_APP_REDIRECT_URI",
+            "valueFrom" : "${aws_ssm_parameter.kako_app_redirect_uri.name}"
+          },
+          {
+            "name" : "KAKO_APP_CLIENT_ID",
+            "valueFrom" : "${aws_ssm_parameter.kako_app_client_id.name}"
+          },
         ],
         "logConfiguration" : {
           "logDriver" : "awslogs",
@@ -81,11 +189,11 @@ resource "aws_alb_target_group" "alb_target_group" {
   vpc_id      = aws_vpc.vpc.id
   target_type = "instance"
   health_check {
-    path                = "/"
+    path                = "/actuator/health"
     protocol            = "HTTP"
     port                = "traffic-port"
-    interval            = 30
-    timeout             = 5
+    interval            = 300
+    timeout             = 120
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
@@ -113,6 +221,6 @@ resource "aws_ecs_service" "ecs_service" {
   load_balancer {
     target_group_arn = aws_alb_target_group.alb_target_group.arn
     container_name   = "${var.prefix}-container"
-    container_port   = 80
+    container_port   = 8080
   }
 }
